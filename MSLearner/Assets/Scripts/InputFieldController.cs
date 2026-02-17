7498ca69e8c6161871f5e44567c46980a1f0c7c3
fudzich <vsevolod656@gmail.com>
2026-02-17 17:45:50 +0300

    Started working on AtE scene: added all UI elements

---
+++ MSLearner/Assets/Scripts/InputFieldController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class InputFieldController : MonoBehaviour
+{
+    public TMP_InputField mainInputField;
+
+    public string selectedWord; // The word selected for the current game
+    public string currentInput; // The player's current input
+
+    private SceneLoader sceneLoader;
+
+    void Awake()
+    {
+        ResetInput();
+
+        // Select a random word from the list
+        selectedWord = WordList.words[Random.Range(0, WordList.words.Count)];
+        sceneLoader = GetComponent<SceneLoader>();
+
+        Debug.Log("The word is: " + selectedWord);
+    }
+
+    public void CheckInput()
+    {
+        currentInput = mainInputField.text;
+        
+        if (currentInput.ToLower() == selectedWord.ToLower())
+        {
+            Debug.Log("Correct! The word is: " + selectedWord);
+            sceneLoader.LoadASLToEnglish();
+        }
+        else
+        {
+            Debug.Log("Incorrect. Correct word: " + selectedWord + "; You inputed: " + currentInput);
+            ResetInput();
+        }
+    }
+
+    // Call this method to clear the current input
+    public void ResetInput()
+    {
+        mainInputField.text = "";
+        Debug.Log("Input has been reset.");
+    }
+}
